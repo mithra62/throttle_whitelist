1b17e5a493bb4898552e3393d4fc9d1058ade856
Eric Lamb <eric@mithra62.com>
2024-07-01 08:44:55 -0700

    adds ability to ignore logged in users from throttling

--- system/user/addons/throttle_whitelist/Extensions/CoreBoot.php
+++ system/user/addons/throttle_whitelist/Extensions/CoreBoot.php
@@ -9,9 +9,17 @@ class CoreBoot extends AbstractRoute
     public function process()
     {
         if (ee()->config->item('enable_throttling') == 'y') {
+
+            //config based ip array
             $whitelist = ee()->config->item('throttling_whitelist');
             if (is_array($whitelist) && count($whitelist) >= 1) {
                 ee()->db->where_in('ip_address', $whitelist)->delete('throttle');
+            }
+
+            //logged in check
+            if(ee()->config->item('throttle_ignore_logged_in') == 'y' &&
+                ee()->session->userdata('member_id') >= 1) {
+                ee()->db->where('ip_address', ee()->input->ip_address())->delete('throttle');
             }
         }
     }
