76b01591ffeb09802a1d53e330a6de332b2a9b19
Eric Lamb <eric@mithra62.com>
2024-07-01 08:26:53 -0700

    initial import

---
+++ system/user/addons/throttle_whitelist/addon.setup.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'name' => 'Throttle Whitelist',
+    'description' => 'Allows for IP Addresses to not be included with Throttle logic',
+    'version' => '1.0.0',
+    'author' => 'Avenu',
+    'author_url' => 'fdsa',
+    'namespace' => 'Avenu\ThrottleWhitelist',
+    'settings_exist' => false,
+];
